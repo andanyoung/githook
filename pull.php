62e4cdaf64800fecf1adf2edf250c388f336935e
andyoung <1218853253@qq.com>
2019-04-26 17:09:46 +0800

    添加请求格式兼容性

--- pull.php
+++ pull.php
@@ -1,19 +1,25 @@
 <?php
 ini_set('display_errors', 'On');
-require_once 'utils/Log.php';
-$requestBody = file_get_contents("php://input");
-if (!$requestBody) {
-    $requestBody = json_decode($requestBody, true);
-} else {
-    $requestBody = $_POST;
-}
 
 $log_level = isset($_GET['log_level']) ? $_GET['is_log'] : 0;
 $log_level = intval($log_level);
 define('LOG_LEVEL', 0);
 
+require_once 'utils/Log.php';
+
+if ($_SERVER['HTTP_CONTENT_TYPE'] === 'application/json') {
+    $requestBody = json_decode($requestBody, true);
+    $requestBody = file_get_contents("php://input");
+} elseif ($_SERVER['HTTP_CONTENT_TYPE'] === 'application/x-www-form-urlencoded') {
+    $requestBody = $_POST;
+}
+if (!isset($requestBody)) {
+    exit('没有请求参数');
+}
+
+
 Log::DEBUG($requestBody);
-echo 'USER' . "\n";
+echo 'USER: ';
 system('whoami');
 
 $path = isset($_GET['path']) ? urldecode($_GET['path']) : '../' . $requestBody['repository']['name'];
