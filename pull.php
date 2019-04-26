7520f0f2af56ea89115015fdfc92f9455edfc65d
andyoung <1218853253@qq.com>
2019-04-26 13:43:15 +0800

    执行命令需要修改权限

--- pull.php
+++ pull.php
@@ -3,12 +3,17 @@ ini_set('display_errors', 'On');
 require_once 'utils/Log.php';
 $requestBody = file_get_contents("php://input");
 
-Log::DEBUG($_POST);
+Log::DEBUG($requestBody);
 
-Log::DEBUG(system("whoami"));
 
-$path = isset($_get['path']) ? $_get['path'] : '../' . $_POST['repository']['name'];
+$requestBody = json_decode($requestBody);
 
-$shell_res = shell_exec("cd {$path} && git pull");
 
+$path = isset($_get['path']) ? urldecode($_get['path']) : '../' . $requestBody['repository']['name'];
+
+$shell_cmd = "cd {$path} && git pull";
+$shell_res = system($shell_cmd);
+
+echo $shell_cmd . '<br>';
+Log::INFO($shell_cmd);
 Log::INFO($shell_res);
