36f391744821f5c37ec14ad58657abd1f2c277e9
andyoung <1218853253@qq.com>
2019-04-26 13:57:47 +0800

    fix

--- pull.php
+++ pull.php
@@ -3,17 +3,18 @@ ini_set('display_errors', 'On');
 require_once 'utils/Log.php';
 $requestBody = file_get_contents("php://input");
 
-Log::DEBUG($requestBody);
+$is_log = $_GET['is_log'];
+$is_log && Log::DEBUG($requestBody);
 
 
 $requestBody = json_decode($requestBody);
 
 
-$path = isset($_get['path']) ? urldecode($_get['path']) : '../' . $requestBody['repository']['name'];
+$path = isset($_GET['path']) ? urldecode($_GET['path']) : '../' . $requestBody['repository']['name'];
 
 $shell_cmd = "cd {$path} && git pull";
 $shell_res = system($shell_cmd);
 
 echo $shell_cmd . '<br>';
-Log::INFO($shell_cmd);
-Log::INFO($shell_res);
+$is_log && Log::INFO($shell_cmd);
+$is_log && Log::INFO($shell_res);
